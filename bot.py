625f30df72cae478442106eb5719c61bc8d49f22
Master3008 <55414793+Master3008@users.noreply.github.com>
2020-09-02 21:36:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,41 +1,35 @@
 import discord
 import datetime
 import os
-import psycopg2
 client = discord.Client()
 @client.event
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-cur = conn.cursor()
-
-cur.execute('''CREATE TABLE STUDENT
-      (ADMISSION INT PRIMARY KEY     NOT NULL,
-      NAME           TEXT    NOT NULL,
-      AGE            INT     NOT NULL,
-      COURSE        CHAR(50),
-      DEPARTMENT        CHAR(50));''')
 
 async def on_member_update(before, after):
     if str(before.status) == "offline":
         if str(after.status) == "online":
-            print("{} is now {}.".format(after.name,after.status)) 
+            print("{} is now {}.".format(after.name,after.status))
+            file = open('log.txt','a+') 
             currentDT = datetime.datetime.now()
             
-            cur.execute("INSERT INTO STUDENT (ADMISSION,NAME,AGE,COURSE,DEPARTMENT) VALUES (3420, 'John', 18, 'Computer Science', 'ICT')");
-            conn.commit()
-            conn.close()
+            file.write("{} is now {}.       ".format(after.name,after.status))
+            file.write(str(currentDT))
+            file.write("\n")
  
-            
+            file.close()
     if str(before.status) == "online":
         if str(after.status) == "offline":
             print("{} has gone {}.".format(after.name,after.status))
-           
+            file = open('log.txt','a')
             currentDT = datetime.datetime.now()
             
  
-           
+            file.write("{} has gone {}.      ".format(after.name,after.status))
+            file.write(str(currentDT))
+
+            file.write("\n")
+ 
+            file.close()
          
         
             
